43d4fb87e80034408e545b39730a1d2dcfa6127e
PxperMoon <89617671+PxperMoon@users.noreply.github.com>
2023-02-11 19:29:32 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 song = "";
 song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
     song = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -13,7 +19,28 @@ function setup() {
     video = createCapture(VIDEO);
     video.hide();
 
-    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized.");
+}
+
+function gotPoses(results) 
+{
+    if(results.length > 0) 
+{
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + "Right Wrist Y = " + rightWristY);
+}
 }
 
 function draw() {
