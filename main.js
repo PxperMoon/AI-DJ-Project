48ddbee2b4a8650cc312c36b7319b272a9036fd1
PxperMoon <89617671+PxperMoon@users.noreply.github.com>
2023-02-13 22:19:21 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,20 @@
-song = "";
-song2 = "";
-
-leftWristX = 0;
-leftWristY = 0;
-
-rightWristX = 0;
-rightWristY = 0;
-
 function preload() {
-    song = loadSound("music.mp3");
-    song2 = loadSound("music2.mp3");
+
 }
 
 function setup() {
-    canvas = createCanvas(400, 500);
+    canvas = createCanvas(700, 600);
     canvas.center();
-
     video = createCapture(VIDEO);
     video.hide();
-
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on("pose", gotPoses);
-}
-
-function modelLoaded() {
-    console.log("PoseNet is initialized.");
-}
-
-function gotPoses(results) 
-{
-    if(results.length > 0) 
-{
-        console.log(results);
-
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("Right Wrist X = " + rightWristX + "Right Wrist Y = " + rightWristY);
-}
 }
 
 function draw() {
-    image(video, 0, 0, 300, 300);
+    image(video, 200, 200, 250, 200);
+    
+
+    rect(30, 175, 10, 250);
+    rect(670, 175, 10, 250);
+    rect(42, 160, 625, 10);
+    rect(42, 427, 625, 10);
 }
